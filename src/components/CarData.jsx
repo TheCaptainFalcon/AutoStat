2ca8b67d8a4376f8868ee441c37fbba8b5c3ddbf
Joseph Kim <josephkim570@gmail.com>
2020-06-09 04:34:01 -0400

    Added logic to obtain multiple promises of the credit endpoint using different API keys. Currently these values are being passed from state to props in the Credits component. Left commented code, if the current code needs to be refactored into its own component to reduce clutter

--- src/components/CarData.jsx
+++ src/components/CarData.jsx
@@ -2,44 +2,96 @@ import React, { Component } from 'react';
 import axios from 'axios';
 import Credits from './Credits';
 import keys from '../config/keys';
+import _ from 'lodash';
 
 class CarData extends Component {
     constructor(props) {
         super(props);
         this.state = {  
-            credits: ''
+            credits1: [],
+            credits2: []
         }
 
     }
 
-    componentDidMount() {
-        axios.get('http://api.carmd.com/v3.0/credits', {
-            headers: {
-                "content-type":"application/json",
-                "authorization":keys.AUTH_KEY,
-                "partner-token":keys.PARTNER_TOKEN
-            }
-        })
-        .then(res => {
-            this.setState({ credits : res.data })
-            console.log(res.data)
-        })
-        .catch(err => {
-            console.log(err)
-        })
-    }
+    // Use similar logic for other endpoints when credit deductions occur
+    // and if it makes sense to refactor this logic all into the Credits comp.
 
-    // mapCredits() {
-    //     return this.state.credits.map((data, i) => {
-    //         return <Credits obj={data} key={i} />
+    // componentDidMount() {
+    //     axios.get('http://api.carmd.com/v3.0/credits', {
+    //         headers: {
+    //             "content-type":"application/json",
+    //             "authorization":keys.AUTH_KEY1,
+    //             "partner-token":keys.PARTNER_TOKEN1
+    //         }
+    //     })
+    //     .then(res => {
+    //         this.setState({ credits1 : res.data.data })
+    //         console.log(res.data)
+
+    //         // if (Object.values(this.state.credits1) === 0) {
+    //         //     axios.get('http://api.carmd.com/v3.0/credits', {
+    //         //         headers: {
+    //         //             "content-type":"application/json",
+    //         //             "authorization":keys.AUTH_KEY2,
+    //         //             "partner-token":keys.PARTNER_TOKEN2
+    //         //         }
+    //         //     })
+    //         // }
+    //         // this.setState({ credits2 : res.data.data })  
+                  
+    //     })
+    //     .catch(err => {
+    //         console.log(err)
     //     })
     // }
+
+    componentDidMount() {
+        const creditURL1 = axios.get('http://api.carmd.com/v3.0/credits', {
+            headers: {
+                "content-type":"application/json",
+                "authorization":keys.AUTH_KEY1,
+                "partner-token":keys.PARTNER_TOKEN1
+            }
+        })
+        .then(res1 => {
+            this.setState({ credits1 : res1.data.data })
+            console.log(res1.data)
+        })
+        .catch(err1 => {
+            console.log(err1)
+        })
+        
+        const creditURL2 = axios.get('http://api.carmd.com/v3.0/credits', {
+            headers: {
+                "content-type":"application/json",
+                "authorization":keys.AUTH_KEY2,
+                "partner-token":keys.PARTNER_TOKEN2
+            }
+        })
+        .then(res2 => {
+            this.setState({ credits2: res2.data.data})
+            console.log(res2.data)
+        })
+        .catch(err2 => {
+            console.log(err2)
+        })
+
+        axios.all([ creditURL1, creditURL2 ])
+    }
+
+    getCredits() {
+        return <Credits 
+            credits1={Object.values(this.state.credits1)} 
+            credits2={Object.values(this.state.credits2)}
+        />
+    }
 
     render() { 
         return (  
             <div>
-                <p>This is sample data</p>
-                <p># of credits remaining: <Credits credits={this.state.credits}/></p>
+                This is some sample data
+                {this.getCredits()}
             </div>
         );
     }
--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -6,12 +6,18 @@ class Credits extends Component {
         this.state = {  }
     }
     render() { 
+        const creditsAmt1 = this.props.credits1;
+        const creditsAmt2 = this.props.credits2;
+
         return (  
-        
-        <div>
-        
-        </div>
-            
+            <div>
+                <div>
+                    API Key 1: {''} {creditsAmt1} {''} Credits Remaining
+                </div>
+                <div>
+                    API Key 2: {''} {creditsAmt2} {''} Credits Remaining
+                </div>
+            </div>
         );
     }
 }
